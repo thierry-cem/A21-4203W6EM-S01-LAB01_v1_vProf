6aacd3fff2647daf2683c12a3a449f51b163d683
Thierry <t.girouxveilleux@cegepmontpetit.ca>
2021-08-27 15:21:21 -0400

    FCT Stars Index
    : Ajout de la classe Star, le Controller Stars et la View Index affichant la liste des étoiles. Ajout d'un menu de gestion avec bootstrap.

---
+++ StarsShop/StarsShop.Web/Controllers/StarsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarsShop.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarsShop.Web.Controllers
+{
+    public class StarsController : Controller
+    {
+        public IActionResult Index()
+        {
+            this.ViewBag.MyStarList = new List<Star>()
+              {
+                new Star(){Name= "Véga", Magnitude=0.03m, Type="A0Va",},
+                new Star(){Name= "Sirius", Magnitude=-1.47m, Type="A1V",},
+                new Star(){Name= "Conapus", Magnitude=-0.72m, Type="F0Ib",},
+                new Star(){Name= "Alsephina", Magnitude=2.03m, Type="AIV",}
+              };
+
+            return View();
+
+        }
+    }
+}
---
+++ StarsShop/StarsShop.Web/Models/Star.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarsShop.Web.Models
+{
+    public class Star
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public decimal Magnitude { get; set; }
+    }
+}
