c9b3cf729b80a3c461f719460a40774b9fd1b58d
Thierry <t.girouxveilleux@cegepmontpetit.ca>
2021-08-27 15:43:11 -0400

    FCT StarType Index, Create et Validation
    Ajout de la validation côté client et côté serveur StarType et View Create.

---
+++ StarsShop/StarsShop.Web/Controllers/StarTypesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarsShop.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarsShop.Web.Controllers
+{
+    public class StarTypesController : Controller
+    {
+        public IActionResult Index()
+        {
+            this.ViewBag.MyStarTypeList = new List<StarType>()
+                  {
+                    new StarType(){TypeName= "O", Id=1},
+                    new StarType(){TypeName= "F", Id=2}
+                    //https://fr.wikipedia.org/wiki/Type_spectral#Tableau_r%C3%A9capitulatif
+
+                  };
+
+
+            return View();
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(StarType model)
+        {
+
+            if (ModelState.IsValid) return RedirectToAction(nameof(Index));
+            else return View();
+        
+        }
+
+
+    }
+}
---
+++ StarsShop/StarsShop.Web/Models/StarType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarsShop.Web.Models
+{
+    public class StarType
+    {
+        public int Id { get; set; }
+        [Display(Name ="Type Name"), Required(ErrorMessage = "Type Name has to be filled")]
+        public string TypeName { get; set; }
+    }
+}
